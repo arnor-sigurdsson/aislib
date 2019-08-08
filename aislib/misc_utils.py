b3309f9964b7c9b594cddbedf3226d7ca7d5d0dd
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2019-08-08 15:10:58 +0200

    Update order of log string fields in get_logger.

--- aislib/misc_utils.py
+++ aislib/misc_utils.py
@@ -14,7 +14,7 @@ def get_logger(name: str) -> logging.Logger:
 
     handler = logging.StreamHandler()
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        '%(asctime)s - %(levelname)s - %(name)s - %(message)s',
         '%H:%M:%S')
     handler.setFormatter(formatter)
     logger_.addHandler(handler)
