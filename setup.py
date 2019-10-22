44142ca3742f46e5694195f22015811fa421c9e7
Arnor Sigurdsson <arnor.sigurdsson@cpr.ku.dk>
2019-10-22 10:57:38 +0000

    Update requirements. Update padding calculation to have target size rounded up rather than down, to make it compatible to when stride = kernel (otherwise we would get negative padding). Add pre-commit and push hooks.

--- setup.py
+++ setup.py
@@ -9,29 +9,24 @@ from setuptools import setup
 def read(filename):
     filename = os.path.join(os.path.dirname(__file__), filename)
     text_type = type(u"")
-    with io.open(filename, mode="r", encoding='utf-8') as fd:
-        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+    with io.open(filename, mode="r", encoding="utf-8") as fd:
+        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
 
 
 setup(
     name="aislib",
     version="0.1.0",
     url="git@github.com:arnor-sigurdsson/aislib.git",
-    license='MIT',
-
+    license="MIT",
     author="Arnor Ingi Sigurdsson",
     author_email="arnor.sigurdsson@cpr.ku.dk",
-
     description="Various code I snippets / functions I have found useful.",
     long_description=read("README.md"),
-
-    packages=find_packages(exclude=('tests',)),
-
+    packages=find_packages(exclude=("tests",)),
     install_requires=[],
-
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
     ],
 )
