13cef59d7df161b67b074e88a062c12ebdddb50b
Arnor Sigurdsson <arnoring@gmail.com>
2020-07-29 13:30:32 +0200

    Add Mish activation function.

--- aislib/pytorch_modules.py
+++ aislib/pytorch_modules.py
@@ -1,6 +1,7 @@
 import torch
 from torch.nn import Module
 from torch.nn.parameter import Parameter
+import torch.nn.functional as F
 
 
 class Swish(Module):
@@ -19,3 +20,40 @@ class Swish(Module):
 
     def extra_repr(self):
         return "num_parameters={}".format(self.num_parameters)
+
+
+class Mish(Module):
+    """
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+    Shape:
+        - Input: (N, *) where * means, any number of additional
+          dimensions
+        - Output: (N, *), same shape as the input
+    Examples:
+        >>> m = Mish()
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+    """
+
+    def __init__(self):
+        """
+        Init method.
+        """
+        super().__init__()
+
+    def forward(self, input):
+        """
+        Forward pass of the function.
+        """
+        return mish(input)
+
+
+@torch.jit.script
+def mish(input):
+    """
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+    See additional documentation for mish class.
+    """
+    return input * torch.tanh(F.softplus(input))
